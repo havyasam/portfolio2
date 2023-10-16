c9627634a674b37c358631304ad5f75de0c942f8
havyas <havyasagaltha@gmail.com>
2023-10-16 18:55:14 +0530

    added scroll reveal js

---
+++ app.js
@@ -0,0 +1,20 @@
+ScrollReveal({ 
+    reset: true ,
+    distance:'100px',
+    duration:2500,
+    delay:500                    
+});
+ScrollReveal().reveal('.he img',{delay:700,origin:'right',interval:200});
+ScrollReveal().reveal('.main2 h1',{delay:700,origin:'left',interval:200});
+ScrollReveal().reveal('.main2 p',{delay:700,origin:'right',interval:200});
+ScrollReveal().reveal('.main1',{delay:700,origin:'bottom',interval:200});
+ScrollReveal().reveal('.main2 h3',{delay:700,origin:'right',interval:200});
+ScrollReveal().reveal('.navbar',{delay:700,origin:'top',interval:200});
+ScrollReveal().reveal('.languages',{delay:100,origin:'left',interval:200});
+ScrollReveal().reveal('.frameworks',{delay:100,origin:'right',interval:200});
+ScrollReveal().reveal('.tools',{delay:100,origin:'bottom',interval:200});
+ScrollReveal().reveal('.note-project',{delay:100,origin:'right',interval:200});
+ScrollReveal().reveal('.note-project1',{delay:100,origin:'left',interval:200});
+ScrollReveal().reveal('.about',{delay:100,origin:'top',interval:200});
+ScrollReveal().reveal('.main3 img',{delay:100,origin:'left',interval:200});
+ScrollReveal().reveal('.main3 p',{delay:100,origin:'right',interval:200});
