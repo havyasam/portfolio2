558d4fad01a8ab6640681900f877f9b768072100
havyas <havyasagaltha@gmail.com>
2023-10-31 23:39:26 +0530

    made some changes in footer section

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 ScrollReveal({ 
     reset: true ,
     distance:'180px',
+    
     duration:2000,
-    delay:20                  
+    delay:50                 
 });
 ScrollReveal().reveal('.he img',{delay:600,origin:'right',interval:200});
 ScrollReveal().reveal('.main2 h1',{delay:700,origin:'left',interval:200});
@@ -18,4 +19,5 @@ ScrollReveal().reveal('.note-project1',{delay:100,origin:'left',interval:200});
 ScrollReveal().reveal('.about',{delay:100,origin:'top',interval:200});
 ScrollReveal().reveal('.main3 img',{delay:100,origin:'left',interval:200});
 ScrollReveal().reveal('.main3 p',{delay:100,origin:'right',interval:200});
+ScrollReveal().reveal('footer img',{delay:50,duration:20000,distance:'1800px',origin:'left',interval:500});
 
