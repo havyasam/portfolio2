0cdbbcd846d9b2d839817c20e3e6a029b12a6bd9
havyas <havyasagaltha@gmail.com>
2023-11-04 23:31:47 +0530

    added redirect page to the portfolio

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 ScrollReveal({ 
     reset: true ,
     distance:'180px',
-    
     duration:2000,
     delay:50                 
 });
@@ -19,5 +18,11 @@ ScrollReveal().reveal('.note-project1',{delay:100,origin:'left',interval:200});
 ScrollReveal().reveal('.about',{delay:100,origin:'top',interval:200});
 ScrollReveal().reveal('.main3 img',{delay:100,origin:'left',interval:200});
 ScrollReveal().reveal('.main3 p',{delay:100,origin:'right',interval:200});
-ScrollReveal().reveal('footer img',{delay:50,duration:20000,distance:'1800px',origin:'left',interval:500});
+//ScrollReveal().reveal('footer img',{delay:50,duration:20000,distance:'1800px',origin:'left',interval:500});
+ScrollReveal().reveal('#h',{delay:200,origin:'bottom',interval:200});
+ScrollReveal().reveal('#a',{delay:800,origin:'bottom',interval:200});
+ScrollReveal().reveal('#v',{delay:1400,origin:'bottom',interval:200});
+ScrollReveal().reveal('#y',{delay:2000,origin:'bottom',interval:200});
+ScrollReveal().reveal('#aa',{delay:2600,origin:'bottom',interval:200});
+ScrollReveal().reveal('#s',{delay:3200,origin:'bottom',interval:200});
 
